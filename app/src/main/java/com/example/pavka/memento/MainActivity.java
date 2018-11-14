fcaad7ae04663d2ab259c26a8a163061ceccd1fc
paul <pavkascool@gmail.com>
2018-11-14 15:12:52 +0300

    Starting commit of CD from office

--- app/src/main/java/com/example/pavka/memento/MainActivity.java
+++ app/src/main/java/com/example/pavka/memento/MainActivity.java
@@ -1,74 +1,43 @@
 package com.example.pavka.memento;
 
-import android.content.ComponentName;
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.Handler;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    private OdometerService odometer;
-    private boolean bound = false;
-    private ServiceConnection connection = new ServiceConnection() {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            OdometerService.OdometerBinder odometerBinder = (OdometerService.OdometerBinder)service;
-            odometer = odometerBinder.getOdometer();
-            bound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            bound = false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        watchMetrage();
+
     }
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, OdometerService.class);
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
 
     }
     @Override
     protected void onStop() {
         super.onStop();
-        if (bound) {
-            unbindService(connection);
-            bound = false;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+            Intent intent = new Intent(this, Questionnaire.class);
+            startActivityForResult(intent, 1);
         }
     }
 
-    private void watchMetrage() {
-        final TextView distanceView = (TextView)findViewById(R.id.distance);
-        final Handler handler = new Handler();
-        handler.post(new Runnable() {
+    @Override
+    protected void onActivityResult(int request, int response, Intent intent) {
 
-            @Override
-            public void run() {
-                double distance = 0.0;
-                if (odometer!=null) {
-                    distance = odometer.getMeters();
-                }
-                String distanceStr = String.format("%1$, .2f meters", distance);
-                distanceView.setText(distanceStr);
-                handler.postDelayed(this, 5000);
-            }
-        });
     }
-
-
 }
