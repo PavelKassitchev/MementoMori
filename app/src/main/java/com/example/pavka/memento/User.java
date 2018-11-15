e36763062860e54ebeb6b68468627de7fb315788
paul <pavkascool@gmail.com>
2018-11-15 16:23:16 +0300

    new User fileds added from office

--- app/src/main/java/com/example/pavka/memento/User.java
+++ app/src/main/java/com/example/pavka/memento/User.java
@@ -1,4 +1,11 @@
 package com.example.pavka.memento;
 
 public class User {
+
+    public static final int MALE = 1;
+    public static final int FEMALE = 2;
+
+    private int gender;
+    private String birthDate;
+    private int[] userData;
 }
